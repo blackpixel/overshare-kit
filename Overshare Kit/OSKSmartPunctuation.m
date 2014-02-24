12603f92a7ac165a844b3850b7505a683c440540
Rudy Richter <rudy@blackpixel.com>
2014-02-24 18:33:30 -0500

    text format warnings

--- Overshare Kit/OSKSmartPunctuation.m
+++ Overshare Kit/OSKSmartPunctuation.m
@@ -43,7 +43,7 @@ typedef NS_ENUM(NSInteger, OSKQuoteDirection) {
 @implementation OSKSmartPunctuationComposedCharacterItem
 
 - (NSString *)description {
-    return [NSString stringWithFormat:@"%lu, %lu string: %@", self.range.location, self.range.length, self.string];
+    return [NSString stringWithFormat:@"%tu, %tu string: %@", self.range.location, self.range.length, self.string];
 }
 
 @end
